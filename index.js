f415e2274ba57dae575425754dabc5f80050fa25
Rishika-Raveendran <62755481+Rishika-Raveendran@users.noreply.github.com>
2020-04-04 16:09:35 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
-import Page from "./webPage.jsx";
-
-ReactDOM.render(<Page />, document.getElementById("root"));
