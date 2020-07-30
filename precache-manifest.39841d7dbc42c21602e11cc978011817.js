23ca3475b91773305ecbb128f20bfbd76cd9680f
Rishika <rishikarocky14@gmail.com>
2020-07-30 22:46:03 +0530

    Updates

---
+++ precache-manifest.39841d7dbc42c21602e11cc978011817.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ecacce8cda57053f216556f26c67dd",
+    "url": "/Bootcamp/index.html"
+  },
+  {
+    "revision": "aa5936dbbbfaaae18f8d",
+    "url": "/Bootcamp/static/css/2.4c3b882e.chunk.css"
+  },
+  {
+    "revision": "fda0ebabf9e859cbe70f",
+    "url": "/Bootcamp/static/css/main.8ff82579.chunk.css"
+  },
+  {
+    "revision": "aa5936dbbbfaaae18f8d",
+    "url": "/Bootcamp/static/js/2.74420d94.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Bootcamp/static/js/2.74420d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fda0ebabf9e859cbe70f",
+    "url": "/Bootcamp/static/js/main.3dcd7f27.chunk.js"
+  },
+  {
+    "revision": "1e1c3707473ffc00b8ad",
+    "url": "/Bootcamp/static/js/runtime-main.914d963c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Bootcamp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Bootcamp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Bootcamp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Bootcamp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Bootcamp/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2559d9dcab784d28e10eb19ab625c92a",
+    "url": "/Bootcamp/static/media/top.2559d9dc.jpg"
+  },
+  {
+    "revision": "91601a65127cf8f8f1067e386563b7c5",
+    "url": "/Bootcamp/static/media/white flame.91601a65.jpg"
+  }
+]);
